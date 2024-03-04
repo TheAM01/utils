3739be77b4583ac856686cd4237e9621cd691ed4
TheAM01 <onevisionofficial55@gmail.com>
2024-03-04 23:44:15 +0500

    Added cookie functions

--- script.js
+++ script.js
@@ -40,3 +40,33 @@ function buildDate() {
     return formatted;
 
 }
+
+function setCookie(cookieName, cookieValue, expirationDays) {
+    const date = new Date();
+    date.setTime(date.getTime() + (expirationDays*24*60*60*1000));
+    let expires = date.toUTCString();
+    document.cookie = `${cookieName}=${cookieValue}; expires=${expires}; path=/`
+}
+
+function deleteCookie(cookieName) {
+    const date = new Date();
+    date.setTime(date.getTime() + (24*60*60*1000));
+    let expires = date.toUTCString();
+    document.cookie = `${cookieName}=; expires=${expires}; path=/`
+}
+
+function getCookie(cookieName) {
+    let name = `${cookieName}=`;
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let cookies = decodedCookie.split(';');
+    for(let i = 0; i <cookies.length; i++) {
+        let cookie = cookies[i];
+        while (cookie.charAt(0) === ' ') {
+            cookie = cookie.substring(1);
+        }
+        if (cookie.indexOf(name) === 0) {
+            return cookie.substring(name.length, cookie.length);
+        }
+    }
+    return null;
+}
